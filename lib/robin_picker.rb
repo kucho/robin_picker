6a6a6b090d12a8999e190ad71916c4cc6a4d6006
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-12-02 18:22:48 -0500

    Add robin picker logic

--- lib/robin_picker.rb
+++ lib/robin_picker.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
+# RobinPicker generates a homogeneous list based on a Round-Robin approach
 class RobinPicker
+  attr_reader :pools, :size
+
+  def initialize(pools, size)
+    @pools = pools.reject(&:empty?)
+    @size = size
+  end
+
+  def perform
+    max_size = pools.map(&:count).max
+    first = pools.shift
+    first.fill(nil, first.count...max_size) unless first.count == max_size
+    first.zip(*pools).flatten.compact.slice(0, size)
+  end
 end
