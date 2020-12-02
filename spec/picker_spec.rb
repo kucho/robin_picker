a4603ac88b8db1a25063dc1e0b5ea1b09eba9c8b
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-12-02 18:22:00 -0500

    Add picker spec

--- spec/picker_spec.rb
+++ spec/picker_spec.rb
@@ -1,4 +1,53 @@
 # frozen_string_literal: true
 
+require_relative '../lib/robin_picker'
+require 'json'
+
 RSpec.describe RobinPicker do
+  subject { described_class.new(pools, 4).perform }
+
+  def read_json(filename)
+    JSON.parse(File.read("spec/fixtures/#{filename}"), symbolize_names: true)
+  end
+
+  shared_examples_for :articles do
+    let(:pools) do
+      raw_pools = file_content.values
+      raw_pools.map do |pool|
+        pool.map { |article| article[:token].to_i }
+      end
+    end
+
+    it { is_expected.to eq result }
+  end
+
+  describe '#pick' do
+    context 'first scenario' do
+      let(:file_content) { read_json('scenario-1.json') }
+      let(:result) { [345_048] }
+
+      include_examples :articles
+    end
+
+    context 'second scenario' do
+      let(:file_content) { read_json('scenario-2.json') }
+      let(:result) { [790_952, 103_678, 788_138, 802_358] }
+
+      include_examples :articles
+    end
+
+    context 'third scenario' do
+      let(:file_content) { read_json('scenario-3.json') }
+      let(:result) { [103_678, 790_952, 802_358, 788_138] }
+
+      include_examples :articles
+    end
+
+    context 'fourth scenario' do
+      let(:file_content) { read_json('scenario-4.json') }
+      let(:result) { [790_952, 103_678, 802_358, 562_873] }
+
+      include_examples :articles
+    end
+  end
 end
